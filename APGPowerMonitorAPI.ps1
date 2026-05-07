25580f1e02646d72ffc115338c210eb7a7b4ace7
Christian Schindler <cnschindler@gmx.net>
2026-05-07 13:32:43 +0200

    Intial Release

---
+++ APGPowerMonitorAPI.ps1
@@ -0,0 +1,64 @@
+﻿$InfoGraphicsHost = "https://app.23degrees.io/api/v2/content"
+$InfoGraphicsEndpoint = "/{0}/data"
+$PeakHourHost = "https://awareness.cloud.apg.at/api"
+$PeakHourHealthEndpoint = "/healthz"
+$PeakHourStatusEndpoint = "/v1/PeakHourStatus"
+$AuthHeader = @{"Authorization" = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDUwZGMwMjEzNjhkM2YwZDkzZWMxMGQiLCJlbWFpbCI6ImFwZy1hcGlAMjNkZWdyZWVzLmlvIiwic2x1ZyI6ImFwZy1hcGktdXNlciIsInJvbGVzIjpbXSwiaWF0IjoxNjgzMDIwODAyfQ.SuwZlsWe_GopDUhTg3RTaRhOGA0roqeD5UyoTbMyHsQ"}
+
+# Slugs for infographics
+$Slugs = @{
+    "ErzeugungErneuerbarerEnergien" = "onFQVwdMNSZ62rCN-line-erzeugung-erneuerbarer-energien"
+    "Lastfluesse" = "n3vfefqpLyjsQ5s2-line-lastfluesse"
+    "EigenerzeugungVonEnergie" = "dpcgsZKysiXZmwtf-bar-vertical-eigenerzeugung-von-energie-in"
+    "WWoechentlicherEnergieverbrauch" = "K7tley8BjdU1ng8j-bar-vertical-woechentlicher-energieverbrauch"
+    "VerfuegbareEnergie" = "btohciVvwEG8jAGr-donut-verfuegbare"
+    "GaskraftwerkeErzeugungsgrad" = "N8hJwwM6SllaR3bZ-donut-gaskraftwerke-erzeugungsgrad"
+    "ImportEuropaeischerReglerwerte" = "VUGApLOeJOMFkc13-bar-vertical-import-europaeischer-reglerwerte"
+}
+
+function Get-InfographicData
+{
+    param
+    (
+        [string]$Slug
+    )
+
+    $url = "$InfoGraphicsHost$InfoGraphicsEndpoint" -f $Slug
+    try
+    {
+        $response = Invoke-RestMethod -Uri $url -Headers $authHeader
+        return $response.Content | ConvertFrom-Json
+    }
+
+    catch
+    {
+        Write-Error "Fehler beim Abrufen der Infografik-Daten: $_"
+        return $null
+    }
+}
+
+function Get-PeakHourStatus
+{
+    $healthUrl = "$PeakHourHost$PeakHourHealthEndpoint"
+    $statusUrl = "$PeakHourHost$PeakHourStatusEndpoint"
+
+    try
+    {
+        # Überprüfen Sie die Gesundheit der API
+        $healthResponse = Invoke-WebRequest -Uri $healthUrl -UseBasicParsing
+        if ($healthResponse.StatusCode -ne 200) {
+            Write-Error "Die Peak Hour API ist nicht gesund. Statuscode: $($healthResponse.StatusCode)"
+            return $null
+        }
+
+        # Abrufen des Peak Hour Status
+        $statusResponse = Invoke-WebRequest -Uri $statusUrl -UseBasicParsing
+        return $statusResponse.Content | ConvertFrom-Json
+    }
+
+    catch
+    {
+        Write-Error "Fehler beim Abrufen des Peak Hour Status: $_"
+        return $null
+    }
+}
